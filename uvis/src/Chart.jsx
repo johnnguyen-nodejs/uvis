2d1189119c0869d2b114b0a6632bc30e7bb1c6fc
johnnguyen-nodejs <hotro.dacsanhuousao@gmail.com>
2024-11-19 01:11:18 +0700

    Update Chart.jsx

--- uvis/src/Chart.jsx
+++ uvis/src/Chart.jsx
@@ -61,13 +61,13 @@ export const ChartComponent = props => {
                 priceScaleId: '', // set as an overlay by setting a blank priceScaleId
                 // set the positioning of the volume series
                 scaleMargins: {
-                    top: 0.3, // highest point of the series will be 70% away from the top
+                    top: 1, // highest point of the series will be 70% away from the top
                     bottom: 0,
                 },
             });
             volumeSeries.priceScale().applyOptions({
                 scaleMargins: {
-                    top: 0.3, // highest point of the series will be 70% away from the top
+                    top: 1, // highest point of the series will be 70% away from the top
                     bottom: 0,
                 },
             });
